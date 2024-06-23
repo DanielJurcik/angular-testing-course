a4a726b768de44df8f0966e2a799ba5fe53e00ba
Daniel Jurčík <dano.jurcik@gmail.com>
2024-06-23 19:00:20 +0200

    Unit tests for user service

---
+++ src/app/shared/services/users.service.spec.ts
@@ -0,0 +1,42 @@
+import { TestBed } from "@angular/core/testing";
+import { UsersService } from "./users.service";
+import { UserInteface } from "../types/user.interface";
+
+describe('Name of the group', () => {
+  let usersService: UsersService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    })
+
+    usersService = TestBed.inject(UsersService);
+  });
+
+  it('creates a service', () => {
+    expect(usersService).toBeTruthy();
+  });
+
+  describe('addUser', () => {
+    it('should add a user', () => {
+      const user:UserInteface = {
+        id: '3',
+        name: 'Jozef'
+      }
+      usersService.addUser(user)
+      expect(usersService.users).toEqual([user]);
+    });
+  });
+
+  describe('removeUser', () => {
+    it('should remove a user', () => {
+      const user:UserInteface = {
+        id: '3',
+        name: 'Jozef'
+      }
+      usersService.users = [user]
+      usersService.removeUser('3')
+      expect(usersService.users).toEqual([]);
+    });
+  });
+});
