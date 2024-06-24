818bc71bb59e54879395bcfec2991b084f61d5fb
Daniel Jurčík <dano.jurcik@gmail.com>
2024-06-24 23:56:51 +0200

    Creating test for HTTP request

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,24 +1,16 @@
 import { TestBed } from "@angular/core/testing";
 import { UsersService } from "./users.service";
 import { UserInteface } from "../types/user.interface";
-import { UtilsService } from "./utils.service";
 
 describe('Name of the group', () => {
   let usersService: UsersService;
-  let utilsService: UtilsService;
-  // const utilsServiceMock = {
-  //   pluck: jest.fn(),
-  // }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [UsersService, UtilsService
-        // {provide: UtilsService, useValue: utilsServiceMock}
-      ]
+      providers: [UsersService]
     })
 
     usersService = TestBed.inject(UsersService);
-    utilsService = TestBed.inject(UtilsService);
   });
 
   it('creates a service', () => {
@@ -32,7 +24,7 @@ describe('Name of the group', () => {
         name: 'Jozef'
       }
       usersService.addUser(user)
-      expect(usersService.users).toEqual([user]);
+      expect(usersService.users$.getValue()).toEqual([user]);
     });
   });
 
@@ -42,24 +34,9 @@ describe('Name of the group', () => {
         id: '3',
         name: 'Jozef'
       }
-      usersService.users = [user]
+      usersService.users$.next([user])
       usersService.removeUser('3')
-      expect(usersService.users).toEqual([]);
-    });
-  });
-
-  describe('getUsernames', () => {
-    it('should get usernames', () => {
-      jest.spyOn(utilsService, 'pluck');
-      usersService.users = [{id: '3', name: 'Jozef'}];
-      usersService.getUsernames();
-      expect(utilsService.pluck).toHaveBeenCalledWith(
-        usersService.users,
-        'name'
-      );  
-
-      // utilsServiceMock.pluck.mockReturnValue(['foo']);
-      // expect(usersService.getUsernames()).toEqual(['foo']);  
+      expect(usersService.users$.getValue()).toEqual([]);
     });
   });
 });
