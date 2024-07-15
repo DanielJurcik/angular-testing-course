6efd83e47f7ed46179dba34384d3f6fd1d47ec75
Daniel Jurčík <dano.jurcik@gmail.com>
2024-07-15 07:17:20 +0200

    Testing HTTP error

--- src/app/shared/services/api.service.spec.ts
+++ src/app/shared/services/api.service.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from "@angular/core/testing";
 import { ApiService } from "./api.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 import { TagInterface } from "../types/tag.interface";
+import { HttpErrorResponse } from "@angular/common/http";
 
 describe('ApiService', () => {
     let apiService: ApiService;
@@ -57,6 +58,28 @@ describe('ApiService', () => {
             req.flush({ id: '1', name: 'foo'});
             expect(req.request.method).toEqual('POST');
             expect(req.request.body).toEqual({name: 'foo'});
+        });
+
+        it('throws an error if request fails', () => {
+            let actualError : HttpErrorResponse | undefined;
+            apiService.createTag('foo').subscribe({
+                next: () => {
+                    fail('Success should not be called');
+                },
+                error: err => {
+                    actualError = err;
+                }
+            });
+            const req = httpTestingController.expectOne('http://localhost:3004/tags');
+            req.flush('Server error',{
+                status: 422,
+                statusText: 'Unprocessible entity'
+            })
+
+            if (!actualError) throw new Error('Error need to be defined');
+
+            expect(actualError.status).toEqual(422);
+            expect(actualError.statusText).toEqual('Unprocessible entity')
         });
     });
 });
